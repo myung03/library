a5f4b7c77ab572f800f26fa38b423625928453ee
Matthew <matthewyungisworking@gmail.com>
2023-05-12 16:55:38 -0700

    added eslint

---
+++ index.js
@@ -0,0 +1,21 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function helper() {
+    const finished = this.helper();
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${finished}`;
+  };
+  this.helper = function helper() {
+    if (read) {
+      return 'read';
+    } return 'not read yet';
+  };
+}
+
+function addBookToLibrary(book) {
+myLibrary.push(book);
+}
