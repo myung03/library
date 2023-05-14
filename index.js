f897af690eeb551235aab8e3e55f0e16e046bc4a
Matthew <matthewyungisworking@gmail.com>
2023-05-13 22:20:42 -0700

    completed styling of cards + add card page

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -15,9 +13,38 @@ function Book(title, author, pages, read) {
     } return 'not read yet';
   };
 }
-function createBook() {
-    
+
+const myLibrary = [new Book('Demon Slayer', 'Muzan', 302, true), new Book('Hatchet', 'Gary Paulsen', 195, true)];
+
+function initBook(book) {
+  // TODO -> add book to page using html elements
 }
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  myLibrary.forEach((b) => {
+    initBook(book);
+    b.classList.add('book-entry');
+    const grid = document.getElementById('#cards');
+    grid.appendChild(book);
+  });
 }
+
+// eslint-disable-next-line no-unused-vars
+function createBook() {
+  const title = '';
+  const author = '';
+  const pages = 0;
+  const read = '';
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+}
+
+// add book popup functionality
+const popup = document.querySelector('.popup');
+document.getElementById('add').addEventListener('click', () => {
+  popup.classList.add('active');
+});
+document.querySelector('.popup .close-btn').addEventListener('click', () => {
+  popup.classList.remove('active');
+});
